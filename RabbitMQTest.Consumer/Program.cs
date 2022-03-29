8a2ae9aa21caacbb6644628dd8452e1be632d25c
umutdr <umutdr98@gmail.com>
2022-03-29 18:24:59 +0300

    Add project files.

---
+++ RabbitMQTest.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQTest.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("queue-uri"),
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "rawdata", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (object model, BasicDeliverEventArgs ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "ats-rawdata", autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQTest.Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQTest.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("queue-uri"),
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "rawdata", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                int repeatCount = 1000;
+                while (true)
+                {
+                    Console.WriteLine($"Press ENTER to send {repeatCount} items to queue");
+                    Console.ReadLine();
+                    Parallel.For(0, repeatCount, new ParallelOptions() { MaxDegreeOfParallelism = 1 }, (i) =>
+                    {
+                        string message = "*HQ,861551049378680,V1,120000,A,41.2428,N,36.4133,E,000.00,357.96,150322,ffffffff,(13)(25)(25)(80),(195)(26),00000001,2.78,M1,CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600)-CAN(2;419358976;8;FFFFFFFF3E270600),IMU(-128;-2;-5;-126;-1;-4)#";
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "", routingKey: "ats-rawdata", basicProperties: null, body: body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    });
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
